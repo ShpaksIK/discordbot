83199ecffeee98769f7980e49a7a1b38084dfc93
hexisnn <139883045+hexisnn@users.noreply.github.com>
2024-01-30 21:24:32 +0300

    awd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
+
 import MainPage from './pages/main/MainPage'
 
 import styleAll from './assets/style/styleAll.module.css'
@@ -10,8 +8,8 @@ function App() {
   return (
     <div className={styleAll.app}>
       <MainPage />
-=======
->>>>>>> Stashed changes
+    
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useRoutes } from "react-router-dom";
 
@@ -34,20 +32,14 @@ const AppWrapper = () => {
       <Router>
         <App />
       </Router>
-<<<<<<< Updated upstream
-=======
->>>>>>> f79a399b9ce5e18b2293cf50b1dda47034d9f6bc
->>>>>>> Stashed changes
     </div>
   )
 }
 
-<<<<<<< Updated upstream
 export default AppWrapper;
-=======
-<<<<<<< HEAD
+
+
 export default App
-=======
+
 export default AppWrapper;
->>>>>>> f79a399b9ce5e18b2293cf50b1dda47034d9f6bc
->>>>>>> Stashed changes
+
--- server/dist/app.js
+++ server/dist/app.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-<<<<<<< HEAD
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
@@ -15,19 +14,5 @@ app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use(body_parser_1.default.json());
 app.use('/user', user_router_1.userRoute);
 app.listen(8080, () => {
-    console.log('SERVER is running');
-=======
-exports.app = void 0;
-const body_parser_1 = __importDefault(require("body-parser"));
-const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
-const user_route_1 = require("./routes/user-route");
-exports.app = (0, express_1.default)();
-exports.app.use((0, cors_1.default)());
-exports.app.use(body_parser_1.default.urlencoded({ extended: false }));
-exports.app.use(body_parser_1.default.json());
-exports.app.use('/user', user_route_1.userRouter);
-exports.app.listen(8080, () => {
-    console.log('server ok');
->>>>>>> f0cdc09f8700a3f25284c72f3275f981fd5b5a76
+    console.log("SERVER OK");
 });
