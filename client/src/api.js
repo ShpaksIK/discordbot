049d25922d6ee43f467d85616ecac7ded88459e8
hexisnn <139883045+hexisnn@users.noreply.github.com>
2024-01-31 20:16:40 +0300

    j

--- client/src/api.js
+++ client/src/api.js
@@ -1,23 +1,21 @@
-<<<<<<< Updated upstream
+
 const botName = "TeamBot"
 const serversUsingBot = "2000"
 const linkToAuth = "https://discord.com/api/oauth2/authorize?client_id=1197185720020897922&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=identify+guilds+gdm.join+email+guilds.join+connections"
 const linkToInvite = "#"
 
 export {botName, serversUsingBot, linkToAuth, linkToInvite}
-=======
-<<<<<<< HEAD
+
 const botName = "TeamBot"
 const serversUsingBot = "2000"
 const linkToInvite = "#"
 
 export {botName, serversUsingBot, linkToInvite}
-=======
+
 const botName = "TeamBot"
 const serversUsingBot = "2000"
 const linkToAuth = "https://discord.com/api/oauth2/authorize?client_id=1197185720020897922&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&scope=identify+guilds+gdm.join+email+guilds.join+connections"
 const linkToInvite = "#"
 
 export {botName, serversUsingBot, linkToAuth, linkToInvite}
->>>>>>> f79a399b9ce5e18b2293cf50b1dda47034d9f6bc
->>>>>>> Stashed changes
+
