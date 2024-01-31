f6a046ca36ceb07d04bc17eecdfb20773904e7cf
ShpaksIK <120310410+ShpaksIK@users.noreply.github.com>
2024-01-31 20:23:56 +0300

    Add files via upload

---
+++ client/src/pages/documentation/docMain/docBlocks/docInvite/DocInvite.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import style from './style.module.css'
+
+
+function DocInvite() {
+    return (
+        <div className={style.docInvite}>
+            <div className={style.container}>
+                <h3>Добавление на сервер</h3>
+            </div>
+        </div>
+    )
+}
+
+export default DocInvite
